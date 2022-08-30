812f0e4cf7ff5de55526d5a0745748d1797df24e
Mateus Motter <mateus.motter@outlook.com>
2022-08-30 18:34:56 +1000

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById('navigation');
+const mainPicture = document.getElementById('main_picture');
+
+const help = document.getElementById('help');
+const youTube = document.getElementById('youtube');
+
+hamburger.onmouseover = () => {
+    nav.style.display = "block";
+}
+
+nav.onmouseover = () => {
+    nav.style.display = "block";
+}
+
+nav.onmouseout = () => {
+    nav.style.display = "none";
+} 
+
+// event listener to close nav menu on mobile version
+
+window.addEventListener('click', function(e){
+	if (hamburger.contains(e.target)) {
+        return;
+  } else {
+    console.log('clicked outside');
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    }
+  }
+})
+
+//Form help video
+
+help.onmouseover = () => {
+    youTube.style.display = "block";
+    youTube.style.position = "absolute";
+}
+
+help.onmouseout = () => {
+    youTube.style.display = "none";
+}
